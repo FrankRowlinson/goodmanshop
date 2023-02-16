1cd3c18ca93c9cbc8310a17d3cb4da1c0ab578fc
Vladislav Smolov <johk2293@gmail.com>
2023-02-16 11:44:50 +0400

    Add not found page text

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,12 @@
+import { Typography } from "../../components"
 import "./NotFoundPage.css"
 
 export function NotFoundPage() {
-  return <div>NotFoundPage</div>
+  return (
+    <div>
+      <Typography variant='regular' size='md'>
+        Что-то пошло не так. Данной страницы не существует.
+      </Typography>
+    </div>
+  )
 }
